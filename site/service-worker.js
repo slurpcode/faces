bc698ee23e5b3600312f5568a8f565c69be3dab0
John Bampton <jbampton@users.noreply.github.com>
2018-08-04 01:23:36 +1000

    Test another image size

--- site/service-worker.js
+++ site/service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v40';
-const RUNTIME = 'runtime-v40';
+const PRECACHE = 'precache-v41';
+const RUNTIME = 'runtime-v41';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
