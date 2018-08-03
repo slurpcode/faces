0be9c3d76607ef4549d6380369c96877d485ddb5
John Bampton <jbampton@users.noreply.github.com>
2018-08-04 00:02:46 +1000

    Fxi path

--- site/service-worker.js
+++ site/service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v32';
-const RUNTIME = 'runtime-v32';
+const PRECACHE = 'precache-v35';
+const RUNTIME = 'runtime-v35';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
